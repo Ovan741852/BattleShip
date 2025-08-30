0d493b7860d95876c13c57de626fb2ec4a457efc
Pika <linaomasa929@gmail.com>
2025-08-30 15:53:02 +0800

    1

--- Assets/Scripts/Impl/FindApple.cs
+++ Assets/Scripts/Impl/FindApple.cs
@@ -1,5 +1,6 @@
 ﻿using GamePlay.UnitData;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 namespace GamePlay
@@ -15,31 +16,20 @@ namespace GamePlay
 
         public bool CanUse(UnitBase unit)
         {
-            var perceptionData = unit.GetUnitData<PerceptionData>();
-            return perceptionData != null;
+            return unit.HasUnitData<PerceptionData>();
         }
 
         public void Execute(UnitBase unit)
         {
-            var self = unit.GetUnitData<TransformData>();
-            unit.RemoveUnitData<DestinationData>();
-            var perceptionData = unit.GetUnitData<PerceptionData>();
-            float shortest = float.MaxValue;
-            UnitBase nearest = null;
-            foreach(var target in perceptionData.value.targets)
-            {
-                var transformData = target.GetUnitData<TransformData>();
-                var targetPos = transformData.value.position;
-                var distance = Vector3.Distance(self.value.position, targetPos);
-                if (distance > shortest)
-                    continue;
-                shortest = distance;
-                nearest = target;
-            }
-            unit.AddUnitData(new FindAppleData()
-            {
-                target = nearest
-            });
+            var perception = unit.GetData<PerceptionData>();
+            var self = unit.GetData<TransformData>();
+
+            // 簡化後的邏輯
+            var nearest = perception.targets
+                .OrderBy(t => Vector3.Distance(self.position, t.GetData<TransformData>().position))
+                .FirstOrDefault();
+
+            unit.SetData(new FindAppleData { target = nearest });
         }
     }
 
