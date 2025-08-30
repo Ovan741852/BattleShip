1b58ff039444ab0581a9795881f6ea41f60e230b
Pika <linaomasa929@gmail.com>
2025-08-30 15:46:24 +0800

    13

---
+++ Assets/Scripts/Impl/EatApple.cs
@@ -0,0 +1,40 @@
+﻿using GamePlay.UnitData;
+using System.Collections;
+using UnityEngine;
+
+namespace GamePlay
+{
+    public class EatApple : IThinkRule
+    {
+        public int priority => 2;
+
+        public bool CanUse(UnitBase unit)
+        {
+            var transform = unit.GetUnitData<TransformData>();
+            var findAppleData = unit.GetUnitData<FindAppleData>();
+            if (findAppleData == null)
+                return false;
+            var firstElement = findAppleData.value.target;
+            var targetTransform = firstElement.GetUnitData<TransformData>();
+            return Vector3.Distance(transform.value.position, targetTransform.value.position) < 0.1f;
+        }
+
+        public void Execute(UnitBase unit)
+        {
+            var findAppleData = unit.GetUnitData<FindAppleData>();
+            var target = findAppleData.value.target;
+            target.AddUnitData(new KillData());
+            unit.RemoveUnitData<FindAppleData>();
+        }
+    }
+
+    public class EatAppleConfigurator : IBehaviorConfigurator
+    {
+        public void ConfigureBehavior(BrainBase brain, UnitBase unit)
+        {
+            if (!unit.HasUnitData<MovementData>())
+                return;
+            brain.AddThinkRule(new EatApple());
+        }
+    }
+}
---
+++ Assets/Scripts/Impl/KillSystem.cs
@@ -0,0 +1,28 @@
+﻿using GamePlay.UnitData;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GamePlay
+{
+    public struct KillData : IUnitData
+    {
+
+    }
+
+    public class KillSystem : SystemBase
+    {
+        protected override Type[] GetRequiredDataTypes()
+        {
+            return new Type[] {
+                typeof(KillData),
+            };
+        }
+
+        protected override void OnUpdateUnit(float deltaTime, UnitBase unit)
+        {
+            unit.RemoveUnitData<KillData>();
+            RemoveUnit(unit);
+        }
+    }
+}
